5c0d66c19d928e8532505f51859f645b2ebff753
Othman Na <othman.nazhran@gmail.com>
2020-07-15 15:48:04 +0100

    first commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../moduls/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private http: HttpClient) {}
+
+   findAll() {
+   return this.http.get<Task[]>('http://localhost:5000/tasks');
+     }
+}
